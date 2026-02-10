6117584a22c43a5a883e5df6d23127d89f973e1d
luowen <374285130@qq.com>
2026-02-10 08:26:17 +0800

    初始化boot项目

---
+++ src/test/java/com/my/bootlearn/BootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.bootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
